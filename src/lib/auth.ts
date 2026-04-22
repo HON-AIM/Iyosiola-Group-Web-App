5088405e6638f063341b458e5df03c958a5f8e89
Israel Miracle <israelmiracle12@gmail.com>
2026-04-22 16:07:20 +0100

    fix: import AuthOptions from next-auth as combined import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,4 @@
-import NextAuth from "next-auth";
-import type { AuthOptions } from "next-auth";
+import NextAuth, { type AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
